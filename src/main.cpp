84ceb492806bd75c004140506598165b79299ae4
Julien Loiseau <contact@julien-loiseau.fr>
2018-08-30 09:41:42 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include "instance.hpp"
 
 int main() {
-#pragma omp parallel
-#pragma omp single
-    std::cout << omp_get_num_threads() << std::endl;
+//#pragma omp parallel
+//#pragma omp single
+//    std::cout << omp_get_num_threads() << std::endl;
 
     instance<8, long long> t;
     auto res = t.solve();
